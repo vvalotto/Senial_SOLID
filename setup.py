eca5175291704ac4f749651b0d6fed581e70f607
Admin <vvalotto@gmail.com>
2017-10-13 18:04:34 -0300

    Ejemplo de SRP - Desarrollado - Todos los módulos en un solo paquete

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from codecs import open
 
 setup(
     name='SenialSOLID',
-    version='0.0.1',
-    description='SenialSOLID: Violacion la principio de SRP',
+    version='2.0.1-dev',
+    description='SenialSOLID: Aplicacion de l Principio SRP',
     author='VV',
     author_email='vvalotto@gmail.com',
     packages=['senial_solid'],
-    py_modules=['lanzador']
+    py_modules=['lanzador'],
+    entry_point={'console_scripts' :
+                 'lanzador = lanzador:Lanzador.ejecutar'}
 )
