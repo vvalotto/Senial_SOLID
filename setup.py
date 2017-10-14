2f2c429e3dab45a4cbe6656b91595c67fdb8e565
Admin <vvalotto@gmail.com>
2017-10-14 14:52:10 -0300

    Correccion del nro de Version del programa - 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs import open
 
 setup(
     name='SenialSOLID',
-    version='0.0.1',
+    version='1.0.0',
     description='SenialSOLID: Violacion la principio de SRP',
     author='VV',
     author_email='vvalotto@gmail.com',
